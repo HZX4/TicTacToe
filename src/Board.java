9477ac1eb81a375f1bce5d7d313a89a46f64419c
HZX4 <33665348+HZX4@users.noreply.github.com>
2017-12-07 13:51:32 -0500

    Minor additions

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,19 @@
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
-public class Board {
+public class Board extends JPanel{
 
+	private JButton[] space;
+	
+	public Board() {
+		
+		//array of buttons as spaces
+		space = new JButton[9];
+		
+		//initialize each element in the array
+		for(JButton b : space)
+			b = new JButton();
+		
+	}
+	
 }
--- src/TicRunner.java
+++ src/TicRunner.java
@@ -1,10 +1,35 @@
 import javax.swing.JFrame;
 
 public class TicRunner extends JFrame{
+	
+	/***
+	 * Originally written by Haylee Thomas
+	 * 7 December 2017 ©
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new TicRunner();	
 	}
+	
+	public TicRunner() {
+		
+		//sets window size
+		setSize(300,300);
+		
+		//set close operation
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//puts the window in the middle of the screen at start
+		setLocationRelativeTo(null);
+		
+		//adds Board object to frame (Board extends JPanel)
+		add(new Board());
+		
+		//open window
+		setVisible(true);
+		
+	}
+	
 
 }
